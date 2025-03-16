98932c5786be8098a20db84c2f35c6ed20e1d9e9
lyc4nthrope <osoriocristhian86@gmail.com>
2025-03-16 09:44:01 -0500

    update todo XD

---
+++ proyectoFinalEDD/src/main/java/co/edu/uniquindio/proyectofinaledd/ProyectoFinalEddApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.proyectofinaledd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinalEddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoFinalEddApplication.class, args);
+    }
+
+}
---
+++ proyectoFinalEDD/src/main/java/co/edu/uniquindio/proyectofinaledd/controller/indexController.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.proyectofinaledd.controller;
+
+import jakarta.faces.view.ViewScoped;
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+@Component
+@Data
+@ViewScoped //solamente una vista, no se si quieren asi toca cambaarlo?
+public class indexController {
+
+
+}
